b2713bc9640f3192c7aa16801d5295a64b7d222b
Ryan McNeely <ryan.mcneely@pm.me>
2019-05-09 00:36:51 -0400

    New NavBar component with included components.

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProjectLogo from '../ProjectLogo/ProjectLogo'
+import ProjectMenus from '../ProjectMenus/ProjectMenus'
+
+export default class NavBar extends React.Component {
+	render() {
+			let props = this.props
+			return(
+				<nav>
+					<ProjectLogo url={props.logoResource}/>
+					<h1>{props.projectTitle}</h1>
+					<ProjectMenus />
+				</nav>
+			)
+	}
+}
---
+++ src/ProjectLogo/ProjectLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class ProjectLogo extends React.Component {
+		render() {
+		return(
+		<img src={this.props.logoResource}/>
+		)
+		}
+}
