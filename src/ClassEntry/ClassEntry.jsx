73ebcbe51d14bcbc74a7840fb22e1360087981e6
Ryan <ryan@Katrinas-MacBook-Pro-2.local>
2019-05-14 23:42:20 -0400

    Starts work onthe class list sidebar feature

---
+++ src/ClassEntry/ClassEntry.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ClassEntry extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div></div>
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/ClassList/ClassList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ClassList extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <aside>
+                    <ClassEntry />
+                </aside>
+            </React.Fragment>
+        )
+    }
+}
