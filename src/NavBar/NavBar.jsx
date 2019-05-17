597cd3f0b7a4c8f690aeb7b4bcf56e17f632b6d4
Ryan McNeely <ryan.mcneely@pm.me>
2019-05-17 19:11:03 -0400

    Base CSS styling.

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ProjectLogo from '../ProjectLogo/ProjectLogo'
 import ProjectMenus from '../ProjectMenus/ProjectMenus'
+import './nav-bar.css'
 
 export default class NavBar extends React.Component {
   render () {
     let props = this.props
     return (
-      <nav>
-        <ProjectLogo url={props.logoResource} />
-        <h1>{props.projectTitle}</h1>
-        <ProjectMenus />
-      </nav>
+<>
+  <nav>
+    <ProjectLogo url={props.logoResource} />
+    <h1>{props.projectTitle}</h1>
+    <ProjectMenus />
+  </nav>
+</>
     )
   }
 }
--- src/containers/DocumentationTemplate.jsx
+++ src/containers/DocumentationTemplate.jsx
@@ -4,11 +4,11 @@ import NavBar from '../NavBar/NavBar'
 export default class DocumentationTemplate extends React.Component {
   render () {
     return (
-      <div>
+      <React.Fragment>
         <NavBar logoResource={''} projectTitle={'Demo Project'} />
         <div>Hello documentation!</div>
         <div>"Wow, they really are strict about that backslash"</div>
-      </div>
+      </React.Fragment>
     )
   }
 }
